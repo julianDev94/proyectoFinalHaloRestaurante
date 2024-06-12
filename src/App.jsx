393508064866e85bbb1615731a970ff2532cb7c0
JulianAballay <juliancabj1210@gmail.com>
2024-06-12 00:35:13 -0300

    agrego el logo del proyecto en el menu mediante una variable, ademas agrego iconos del carrito y usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 import Menu from './components/common/Menu'
 
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,56 +1,53 @@
-import {
-    Nav,
-    Container,
-    Navbar,
-    NavDropdown,
-    Button
-  } from "react-bootstrap";
+import { Nav, Container, Navbar, NavDropdown, Button } from "react-bootstrap";
+import Logo from "../../assets/FONDO BLANCO.svg";
+const Menu = () => {
+  return (
+    <Navbar expand="lg" data-bs-theme="dark" className="py-1 fuenteMenu">
+      <Container className="d-flex justify-content-around">
+        <Navbar.Brand href="#home">
+          <img
+            src={Logo}
+            className="img-fluid"
+            alt="Imagen del logo Halo restaurante"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#">Nuestros platos</Nav.Link>
+            <Nav.Link href="#">Nosotros</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <NavDropdown title="Administrador">
+              <NavDropdown.Item href="#">Panel de productos</NavDropdown.Item>
+              <NavDropdown.Item href="#">Listado de usuarios</NavDropdown.Item>
+              <NavDropdown.Item href="#">Panel de pedidos</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Usuario">
+              <NavDropdown.Item href="#">Mi información</NavDropdown.Item>
+              <NavDropdown.Item href="#">Pedidos</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">
+              <a class="iconosMenu" href="#">
+                <i className="bi bi-person-fill-add iconosMenu"></i>
+              </a>
+            </Nav.Link>
+            <Nav.Link href="#">
+              <a class="iconosMenu" href="#">
+                <i className="bi bi-cart4 iconosMenu"></i>
+              </a>
+            </Nav.Link>
+            <Nav.Link href="#">
+              <Button className="botonMenu">
+                <i class="bi bi-whatsapp"></i>
+              </Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-  
-  const Menu = () => {
-    return (
-      <Navbar expand="lg" data-bs-theme="dark" className="py-1 fuenteMenu">
-        <Container className="d-flex justify-content-around">
-          <Navbar.Brand href="#home">
-           {/*
-            <img
-              src={Logo}
-              className="img-fluid"
-              width={100}
-              alt="Imagen del logo Halo restaurante"
-            />*/
-           } 
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="m-auto">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <Nav.Link href="#">Nuestros platos</Nav.Link>
-              <Nav.Link href="#">Nosotros</Nav.Link>
-            </Nav>
-            <Nav className="ms-auto">
-              <Nav.Link href="#">
-                <Button variant="outline-light">
-                <i class="bi bi-person-fill-add"></i>
-                </Button>
-              </Nav.Link>
-              <Nav.Link href="#">
-                <Button variant="light">Regístrate</Button>
-              </Nav.Link>
-              <NavDropdown title="Administrador">
-                <NavDropdown.Item href="#">Panel de productos</NavDropdown.Item>
-                <NavDropdown.Item href="#">Listado de usuarios</NavDropdown.Item>
-                <NavDropdown.Item href="#">Panel de pedidos</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="Usuario">
-                <NavDropdown.Item href="#">Mi información</NavDropdown.Item>
-                <NavDropdown.Item href="#">Pedidos</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  };
-  
-  export default Menu;
+export default Menu;
