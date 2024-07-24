f5d88c7ca9ba888a1606f44e216dcf34db09e12c
JulianAballay <juliancabj1210@gmail.com>
2024-07-24 00:26:53 -0300

    agrego la seccion de novedades, esto es una base para seguir maquetando

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,5 +1,5 @@
 import logo from "../../assets/logoFondoBlanco.svg";
-import { Container, Row,Col } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 
 const Inicio = () => {
   return (
@@ -23,30 +23,16 @@ const Inicio = () => {
       </article>
       <article className="text-center my-3">
         <Container>
-          <h2 className="">RESEÑAS DE CLIENTES</h2>
-          <Row>
-            <Col>
-              <div
-                data-bs-spy="scroll"
-                data-bs-target="#simple-list-example"
-                data-bs-offset="0"
-                data-bs-smooth-scroll="true"
-                className="scrollspy-example"
-                tabindex="0"
-              >
-                <h4 id="simple-list-item-1">Item 1</h4>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quibusdam repellendus eligendi voluptates neque, nam impedit id blanditiis ducimus aut odio. Exercitationem ea aliquid, molestias corrupti explicabo molestiae beatae maxime.</p>
-                <h4 id="simple-list-item-2">Item 2</h4>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quibusdam repellendus eligendi voluptates neque, nam impedit id blanditiis ducimus aut odio. Exercitationem ea aliquid, molestias corrupti explicabo molestiae beatae maxime.</p>
-                <h4 id="simple-list-item-3">Item 3</h4>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quibusdam repellendus eligendi voluptates neque, nam impedit id blanditiis ducimus aut odio. Exercitationem ea aliquid, molestias corrupti explicabo molestiae beatae maxime.</p>
-                <h4 id="simple-list-item-4">Item 4</h4>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quibusdam repellendus eligendi voluptates neque, nam impedit id blanditiis ducimus aut odio. Exercitationem ea aliquid, molestias corrupti explicabo molestiae beatae maxime.</p>
-                <h4 id="simple-list-item-5">Item 5</h4>
-                <p>...</p>
-              </div>
-            </Col>
-          </Row>
+          <h2 className="fw-bold">RESEÑAS DE CLIENTES</h2>
+        </Container>
+      </article>
+      <article className="text-center my-3 bg-primary">
+        <h2 className="text-light fw-bold">NOVEDADES</h2>
+        <Container className="carousel-novedades">
+            <div className="novedades-images"><Image src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostadas-de-naranja-con-pistachos/900/tostadas-de-naranja-con-pistachos_version_1712721714.jpg"></Image></div>
+            <div className="novedades-images"><Image src=""></Image></div>
+            <div className="novedades-images"><Image src=""></Image></div>
+            <div className="novedades-images"><Image src=""></Image></div>
         </Container>
       </article>
     </section>
