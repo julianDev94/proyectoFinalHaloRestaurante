12ef0c312e534ab1883b12d951cb7bf8d3abfe8e
JulianAballay <juliancabj1210@gmail.com>
2024-08-13 20:52:53 -0300

    agrego un boton que direcciona a la pagina de menu del restaurante y ademas cambio el orden de los articulos de la pagina

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import logo from "../../assets/logoFondoBlanco.svg";
-import { Container, Card, Row, Col } from "react-bootstrap";
+import { Container, Card, Row, Col, Button } from "react-bootstrap";
 
 const Inicio = () => {
   useEffect(() => {
@@ -41,7 +41,7 @@ const Inicio = () => {
           <Row>
             <Col sm={12} xl={6}>
               <Card className="shadow-lg text-center border-0 h-100 rounded">
-                <Card.Body className="d-flex align-items-center">
+                <Card.Body className="d-flex flex-column justify-content-center">
                   <Card.Text className="fs-3 p-4">
                     Descubre Halo, tu destino para disfrutar de deliciosos
                     platos saludables y llenos de sabor. En nuestro restaurante,
@@ -51,6 +51,7 @@ const Inicio = () => {
                     superalimentos hasta platos gourmet, en Halo encontrarás una
                     experiencia gastronómica que nutre cuerpo y alma.
                   </Card.Text>
+                  <Button variant="success" className="fs-2">Nuestros platos aqui!</Button>
                 </Card.Body>
               </Card>
             </Col>
@@ -64,7 +65,56 @@ const Inicio = () => {
           </Row>
         </Container>
       </article>
-      <article className="text-center">
+      
+      <article className="text-center mt-3 novedades-seccion">
+        <h2 className="text-light fw-bold py-3">NOVEDADES</h2>
+        <Container className="d-flex">
+          <div className="scrolling-button-container">
+            <span className="text-light fs-1" id="scrolling-button-left">
+              <i className="bi bi-chevron-left"></i>
+            </span>
+          </div>
+          <div id="scrolling-container">
+            <div className="scrolling-image">
+              <img
+                className="w-100 rounded"
+                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/bowl-de-avena-con-arandanos-platano-y-crema-de-cacahuete/900/bowl-de-avena-con-arandanos-platano-y-crema-de-cacahuete_version_1713499310.jpg"
+                alt="Imagen de bowl de avena con arandanos, platano y crema de cacahuate"
+              />
+            </div>
+            <div className="scrolling-image">
+              <img
+                className="w-100 rounded"
+                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostada-de-queso-con-fresas-y-platano/900/tostada-de-queso-con-fresas-y-platano_version_1710993711.jpg"
+              ></img>
+            </div>
+            <div className="scrolling-image">
+              <img
+                className="w-100 rounded"
+                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostadas-de-naranja-con-pistachos/900/tostadas-de-naranja-con-pistachos_version_1712721714.jpg"
+              ></img>
+            </div>
+            <div className="scrolling-image">
+              <img
+                className="w-100 rounded"
+                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostadas-de-naranja-con-pistachos/900/tostadas-de-naranja-con-pistachos_version_1712721714.jpg"
+              ></img>
+            </div>
+            <div className="scrolling-image">
+              <img
+                className="w-100 rounded"
+                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostadas-de-naranja-con-pistachos/900/tostadas-de-naranja-con-pistachos_version_1712721714.jpg"
+              ></img>
+            </div>
+          </div>
+          <div className="scrolling-button-container">
+            <span className="text-light fs-1" id="scrolling-button-right">
+              <i className="bi bi-chevron-right"></i>
+            </span>
+          </div>
+        </Container>
+      </article>
+      <article className="text-center container-resenias">
         <Container>
           <h2 className="fw-bold">RESEÑAS DE CLIENTES</h2>
           <div className="scroll-container-resenias">
@@ -130,55 +180,7 @@ const Inicio = () => {
             </div>
           </div>
         </Container>
-      </article>
-      <article className="text-center my-3 novedades-seccion">
-        <h2 className="text-light fw-bold py-3">NOVEDADES</h2>
-        <Container className="d-flex">
-          <div className="scrolling-button-container">
-            <span className="text-light fs-1" id="scrolling-button-left">
-              <i className="bi bi-chevron-left"></i>
-            </span>
-          </div>
-          <div id="scrolling-container">
-            <div className="scrolling-image">
-              <img
-                className="w-100 rounded"
-                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/bowl-de-avena-con-arandanos-platano-y-crema-de-cacahuete/900/bowl-de-avena-con-arandanos-platano-y-crema-de-cacahuete_version_1713499310.jpg"
-                alt="Imagen de bowl de avena con arandanos, platano y crema de cacahuate"
-              />
-            </div>
-            <div className="scrolling-image">
-              <img
-                className="w-100 rounded"
-                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostada-de-queso-con-fresas-y-platano/900/tostada-de-queso-con-fresas-y-platano_version_1710993711.jpg"
-              ></img>
-            </div>
-            <div className="scrolling-image">
-              <img
-                className="w-100 rounded"
-                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostadas-de-naranja-con-pistachos/900/tostadas-de-naranja-con-pistachos_version_1712721714.jpg"
-              ></img>
-            </div>
-            <div className="scrolling-image">
-              <img
-                className="w-100 rounded"
-                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostadas-de-naranja-con-pistachos/900/tostadas-de-naranja-con-pistachos_version_1712721714.jpg"
-              ></img>
-            </div>
-            <div className="scrolling-image">
-              <img
-                className="w-100 rounded"
-                src="https://d36fw6y2wq3bat.cloudfront.net/recipes/tostadas-de-naranja-con-pistachos/900/tostadas-de-naranja-con-pistachos_version_1712721714.jpg"
-              ></img>
-            </div>
-          </div>
-          <div className="scrolling-button-container">
-            <span className="text-light fs-1" id="scrolling-button-right">
-              <i className="bi bi-chevron-right"></i>
-            </span>
-          </div>
-        </Container>
-      </article>
+      </article> 
     </section>
   );
 };
