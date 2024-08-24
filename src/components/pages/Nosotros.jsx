7c2547542fe015f02f4ae0b6524549c5c647ea9d
JulianAballay <juliancabj1210@gmail.com>
2024-08-24 16:47:08 -0300

    modifico nuevamente el maquetado de la card de manera definitiva

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -1,39 +1,29 @@
-import { Container } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 import ImagenJulian from "../../assets/IMG-JulianAballay.jpg";
 
 const Nosotros = () => {
   return (
     <section className="sectionMain componente-Nosotros">
       <Container className="d-flex justify-content-center align-items-center py-3">
-        <div id="card-Nosotros">
-          <div className="img-julian">
-            <img
-              src={ImagenJulian}
-              alt="Imagen de Julian Aballay"
-              className="w-100"
-            />
-          </div>
-          <div className="p-2">
-            <h3>Julian Aballay</h3>
-            <p>
-              Hola! mi nombre es Julian, estudiante avanzado en ingenieria de
-              sistemas y actualmente adentrandome al desarrollo web Full Stack
-              en Rolling Code School. Apasionado por la tecnologia y por crear
-              soluciones optimas para problemas de cualquier tipo.{" "}
-            </p>
-          </div>
-          <div id="card-footer">
-            <a href="#">
-              <i className="bi bi-github px-1"></i>
-            </a>
-            <a href="#">
-              <i className="bi bi-linkedin px-1"></i>
-            </a>
-          </div>
-        </div>
+        <Card className="card-Julian shadow">
+          <Card.Img variant="top" src={ImagenJulian} />
+          <Card.Body className="text-center">
+            <Card.Title className="fs-3 fw-bold">Julian Aballay</Card.Title>
+            <Card.Text>
+              Hola! mi nombre es Julian, estudiante avanzado en ingeniería de
+              sistemas y actualmente me encuentro adentrandome al desarrollo web Full Stack
+              en Rolling Code School. Apasionado por la tecnologia y por dar soluciones optimas para problemas que requieran del uso de tecnologías.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer className="text-muted text-center fs-3">
+          <a href="#"><i class="bi bi-github mx-2"></i></a>
+          <a href=""><i class="bi bi-linkedin mx-2"></i></a>
+          </Card.Footer>
+        </Card>
       </Container>
     </section>
   );
 };
+
 
 export default Nosotros;
