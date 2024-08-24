894fbb102809d71dcfcbbca034300a4d1039b1b4
JulianAballay <juliancabj1210@gmail.com>
2024-08-23 22:08:31 -0300

    agrego algunas modificaciones de la card

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -1,18 +1,34 @@
 import { Container } from "react-bootstrap";
+import ImagenJulian from "../../assets/IMG-JulianAballay.jpg";
 
 const Nosotros = () => {
   return (
     <section className="sectionMain componente-Nosotros">
-      <Container className="d-flex justify-content-center text-center">
+      <Container className="d-flex justify-content-center align-items-center py-3">
         <div id="card-Nosotros">
-          <div>
-            <p>imagen</p>
+          <div className="img-julian">
+            <img
+              src={ImagenJulian}
+              alt="Imagen de Julian Aballay"
+              className="w-100"
+            />
           </div>
-          <h3>Acerca de mí</h3>
-          <p></p>
-          <div>
-            <a href="#"><i class="bi bi-github"></i></a>
-            <a href="#"><i class="bi bi-linkedin"></i></a>
+          <div className="p-2">
+            <h3>Julian Aballay</h3>
+            <p>
+              Hola! mi nombre es Julian, estudiante avanzado en ingenieria de
+              sistemas y actualmente adentrandome al desarrollo web Full Stack
+              en Rolling Code School. Apasionado por la tecnologia y por crear
+              soluciones optimas para problemas de cualquier tipo.{" "}
+            </p>
+          </div>
+          <div id="card-footer">
+            <a href="#">
+              <i className="bi bi-github px-1"></i>
+            </a>
+            <a href="#">
+              <i className="bi bi-linkedin px-1"></i>
+            </a>
           </div>
         </div>
       </Container>
